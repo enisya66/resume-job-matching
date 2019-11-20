b257176466cad1520929a76a75e0d444a43df933
m.suhendra <m.suhendra@ampada.de>
2019-11-20 18:42:26 +0100

    Add Manhattan LSTM

---
+++ ModelMaLSTM.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 10:36:32 2019
+
+@author: User
+"""
+
+# keras imports
+from keras.layers import Dense, Input, LSTM, Bidirectional, Lambda, Dropout
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers.embeddings import Embedding
+from keras.optimizers import RMSprop, Adam, Adadelta
+from keras.regularizers import l2
+from keras.callbacks import TensorBoard
+from keras.models import load_model
+from keras.models import Model, Sequential
+from keras import backend as K
+from keras.initializers import Constant
+# std imports
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import os
+
+# TODO create train dev set w/o leaks
+from EmbeddingUtils import create_train_dev_set
+
+
+class SiameseMaLSTM:
+    def __init__(self, embedding_dim, max_sequence_length, number_lstm, number_dense, rate_drop_lstm, 
+                 rate_drop_dense, learning_rate, hidden_activation, validation_split_ratio, loss_function):
+        self.embedding_dim = embedding_dim
+        self.max_sequence_length = max_sequence_length
+        self.number_lstm_units = number_lstm
+        self.rate_drop_lstm = rate_drop_lstm
+        self.number_dense_units = number_dense
+        self.learning_rate = learning_rate
+        self.activation_function = hidden_activation
+        self.rate_drop_dense = rate_drop_dense
+        self.validation_split_ratio = validation_split_ratio
+        self.loss_function = loss_function
+
+    
+    def euclidean_distance(self, vects):
+        x, y = vects
+        sum_square = K.sum(K.square(x - y), axis=1, keepdims=True)
+        return K.sqrt(K.maximum(sum_square, K.epsilon()))
+    
+    def manhattan_distance(self, vects):
+        x, y = vects
+        return K.exp(-K.sum(K.abs(x - y), axis=1, keepdims=True))
+    
+    def cosine_distance(self, vects):
+        x, y = vects
+        x = K.l2_normalize(x, axis=-1)
+        y = K.l2_normalize(y, axis=-1)
+        return -K.mean(x * y, axis=-1, keepdims=True)
+    
+    def eucl_dist_output_shape(self, shapes):
+        shape1, shape2 = shapes
+        print('shape1',shape1)
+        print('shape2',shape2)
+        return (shape1[0], 1)
+    
+    def contrastive_loss(self, y_true, y_pred):
+        '''Contrastive loss from Hadsell-et-al.'06
+        http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+        '''
+        margin = 1
+        square_pred = K.square(y_pred)
+        margin_square = K.square(K.maximum(margin - y_pred, 0))
+        return K.mean(y_true * square_pred + (1 - y_true) * margin_square)
+    
+    def init_weights(self, shape, name=None):
+        values = np.random.normal(loc=0,scale=1e-2,size=shape)
+        return K.variable(values, name=name)
+
+    def train_model(self, sentences_pair, categories, tokenizer, embedding_matrix, model_save_directory='./models/'):
+        """
+        Train Siamese network to find similarity between sentences in `sentences_pair`
+            Steps Involved:
+                1. Pass each pair from sentences_pairs to CNN.
+                2. Merge the vectors from CNN and pass to dense layer.
+                3. Pass the dense layer vectors to sigmoid output layer.
+                4. Use cross entropy loss to train weights
+        Args:
+            sentences_pair (list): list of tuple of sentence pairs
+            categories (list): target values (1-5)
+            embedding_meta_data (dict): dict containing tokenizer and word embedding matrix
+            model_save_directory (str): working directory for where to save models
+        Returns:
+            return (best_model_path):  path of best model
+        """
+        train_data_x1, train_data_x2, train_labels, leaks_train, \
+        val_data_x1, val_data_x2, val_labels, leaks_val = create_train_dev_set(tokenizer, sentences_pair,
+                                                                               categories, self.max_sequence_length,
+                                                                               self.validation_split_ratio)
+
+        if train_data_x1 is None:
+            print("++++ !! Failure: Unable to train model ++++")
+            return None
+
+        
+        nb_words = len(tokenizer.word_index) + 1
+
+        # Creating word embedding layer
+        embedding_layer = Embedding(nb_words, self.embedding_dim, weights=[embedding_matrix],
+                                    input_length=self.max_sequence_length, trainable=False)
+
+
+        
+        # LSTM base network
+        
+        lstm_layer = LSTM(self.number_lstm_units)
+        #lstm_layer = Bidirectional(LSTM(self.number_lstm_units, dropout=self.rate_drop_lstm, recurrent_dropout=self.rate_drop_lstm))
+
+
+
+        # Connect LSTM layer for First Sentence
+        sequence_1_input = Input(shape=(self.max_sequence_length,))
+        embedded_sequences_1 = embedding_layer(sequence_1_input)
+        x1 = lstm_layer(embedded_sequences_1)
+
+        # Connect LSTM layer for Second Sentence
+        sequence_2_input = Input(shape=(self.max_sequence_length,))
+        embedded_sequences_2 = embedding_layer(sequence_2_input)
+        x2 = lstm_layer(embedded_sequences_2)
+        
+        # TODO add lambda layer. See MNIST Siamese
+        
+        distance = Lambda(self.manhattan_distance, output_shape=self.eucl_dist_output_shape)([x1, x2])
+        
+        # comment either one out
+        #output = Dense(categories.shape[1], activation='softmax')(distance)
+        output = Dense(1, activation='sigmoid')(distance)
+
+
+        model = Model([sequence_1_input, sequence_2_input], output)
+        
+        # comment either one out
+        #rms = RMSprop(lr=0.0001)
+        adam = Adam(lr=0.0001)
+        adadelta = Adadelta(clipnorm=1.)
+        model.compile(loss=self.loss_function, optimizer='adam', metrics=['accuracy'])
+        #model.compile(loss=self.contrastive_loss, optimizer=rms)
+
+        
+        # print model
+        model.summary()
+
+        early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+
+        STAMP = 'cnn_%d_%d_%.2f_%.2f' % (self.number_lstm_units, self.number_dense_units, self.rate_drop_lstm, self.rate_drop_dense)
+
+        checkpoint_dir = model_save_directory + 'checkpoints/' + str(int(time.time())) + '/'
+
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+
+        bst_model_path = checkpoint_dir + STAMP + '.h5'
+
+        model_checkpoint = ModelCheckpoint(bst_model_path, save_best_only=True, save_weights_only=False)
+
+        tensorboard = TensorBoard(log_dir=checkpoint_dir + "logs/{}".format(time.time()))
+
+        history = model.fit([train_data_x1, train_data_x2], train_labels,
+                  validation_data=([val_data_x1, val_data_x2], val_labels),
+                  epochs=8, batch_size=64, shuffle=True, verbose=1,
+                  callbacks=[early_stopping])
+        
+        plt.plot(history.history['loss'], 'bo', label='Loss')
+        plt.plot(history.history['val_loss'], 'b', label='Validation')
+        plt.xlabel('epochs')
+        plt.ylabel('loss')
+        plt.show()
+
+        return model
